829f69470af7654fdece7817f7b34d02aebd5af8
Tanishq Singh <>
2022-01-18 08:56:41 +0530

    #2

--- pages/api/invoice/download/application.ts
+++ pages/api/invoice/download/application.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import verifyToken from '../../../../lib/verifyToken';
 import type { verifyTokenReturn } from '../../../../lib/verifyToken';
-import INVOICE from '../../../../database/models/invoice';
+import { INVOICE } from '../../../../database/models/invoice';
 import connectToDB from '../../../../database/db';
 import pdf from 'html-pdf';
 
@@ -27,12 +27,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
         case "GET":
             await connectToDB();
 
-            await INVOICE.findById(query._id)
-                .then(result => {
-                    if (result === null)
-                        return res.status(200).json({ error: true, message: [{ operation: "_id", message: "_id not valid" }] })
+            try {
+                await INVOICE.findById(query._id)
+                    .then(result => {
+                        if (result === null)
+                            return res.status(200).json({ error: true, message: [{ operation: "_id", message: "_id not valid" }] })
 
-                    try {
                         const html = application(result);
                         pdf.create(
                             html, {
@@ -43,13 +43,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
                             stream.pipe(res);
                             return res.end();
                         })
-                    } catch (error) {
-                        res.status(200).json({ error: true, message: [{ operation: "unknown-error", message: error }] })
-                    }
 
-                    // return res.status(200).json({ error: false, data: result })
-                })
-                .catch(err_ => res.status(200).json({ error: true, message: [{ operation: "unknown-error", message: err_.code }] }));
+                        // return res.status(200).json({ error: false, data: result })
+                    })
+                    .catch(err_ => res.status(200).json({ error: true, message: [{ operation: "unknown-error", message: err_.code }] }));
+            } catch (error) {
+                res.status(200).json({ error: true, message: [{ operation: "unknown-error", message: error }] })
+            }
 
             break;
 
